afb5f8c7ef4422d88c89fb6d38ab793cefccfd1a
Conor Green <conorgreen@mmc.school.nz>
2019-07-26 10:41:38 +1200

    Set up basic integers and lists for later use. introduced how to find the resource for the image as well as how to restore the image after it has been rotated

--- Star_Trooper/Form1.cs
+++ Star_Trooper/Form1.cs
@@ -14,11 +14,76 @@ namespace Star_Trooper
 {
     public partial class Form1 : Form
     {
+        Random RandomNumber;  // create a new random number generator
+        Bitmap OGUserImage; // create a new variable as the orignal ship image incase ship image is damaged during rotation
+        Bitmap UserImage; // create a new variable as the ship image 
+        User myUser; // create new variable called my ship
+        Bitmap OGEnemyImage; // create a new vairable called original Enemy image incase Enemy image gets damaged during rotation
+        Bitmap OGMissileImage; // create a new variable called original bullet image incse bullet image gets damaged during rotation
+        List<Enemy> EnemyList; // create a new list variable called Enemylist ( this is to hold the many Enemies used on the form)
+        List<Missile> MissileList; // create a new list for the bullets on the game panel and call it bullet list
+
+        int EnemySpeed;
+
+        int Points, lives;
+
+        const int maxEnemies = 3;
+
         public Form1()
         {
             InitializeComponent();
-        }
+            // Secondary image provides as a base for the computer to reference while the image rotates and pixelates 
 
+            OGUserImage = GetResourceByImageName("Ship");
+            OGUserImage.MakeTransparent(Color.Black);
+
+            OGEnemyImage = GetResourceByImageName("Enemy");
+            OGEnemyImage.MakeTransparent(Color.Black);
+
+            OGMissileImage = GetResourceByImageName("Missile");
+            OGMissileImage.MakeTransparent(Color.Black);
+
+            //create and set the variables for the moving components
+            myUser = new User();
+            myUser.X = 50;
+            myUser.Y = 140;
+            myUser.Angle = 0;
+            UserImage = RotateImage(OGUserImage, myUser.Angle);
+
+            //Make the enemy list
+            EnemyList = new List<Enemy>();
+
+            //make the Bullet List
+            MissileList = new List<Missile>();
+
+            //create the enemy and set up the position
+            RandomNumber = new Random(); // set it as a random spawn function depending on the angle the enemy is spawned in at 
+
+        }
+        public Bitmap GetResourceByImageName(string imageName) // collect the resource using its name as a reference
+        {
+            System.Reflection.Assembly asm = System.Reflection.Assembly.GetExecutingAssembly();
+            string resourceName = asm.GetName().Name + ".properties.Resources";
+            var rm = new System.Resources.ResourceManager(resourceName, asm);
+            return (Bitmap)rm.GetObject(imageName);
+        }
+        private Bitmap RotateImage(Bitmap bmp, float angle)
+        {
+            Bitmap rotatedImage = new Bitmap(bmp.Width, bmp.Height); //create a new bitmap as a rotated image
+            using (Graphics g = Graphics.FromImage(rotatedImage)) // create the new image using graphics
+            {
+                // Set the rotation point to the center in the matrix
+                g.TranslateTransform(bmp.Width / 2, bmp.Height / 2);
+                // Rotate
+                g.RotateTransform(angle);
+                // Restore rotation point in the matrix
+                g.TranslateTransform(-bmp.Width / 2, -bmp.Height / 2);
+                // Draw the image on the bitmap
+                g.DrawImage(bmp, new Point(0, 0));
+            }
+
+            return rotatedImage;
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
